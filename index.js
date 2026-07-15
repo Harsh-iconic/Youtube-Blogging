22d68300337192537e14f07033bec388ba04282d
Harsh-iconic <harshsoalnki4@gmail.com>
2026-07-15 17:30:36 +0530

    Add blog upload functionality: implement multer for file uploads, update blog creation route, and enhance home view to display blogs with cover images

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const Blog = require('./models/model.blog');
+
 const userRoute = require('./routes/route.user');
 const blogRoute = require('./routes/route.blog');
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
@@ -19,10 +21,14 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
+
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user: req.user,
+        blogs: allBlogs,
     });
 })
 
